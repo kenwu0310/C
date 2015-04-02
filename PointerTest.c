707ea3b2ebeb2734db6914760e04d41f40bffcf0
Ken Wu <ken594088@gmail.com>
2015-04-02 09:53:32 +0800

    Update PointerTest.c

--- PointerTest.c
+++ PointerTest.c
@@ -8,7 +8,7 @@ main()
 start:
         system("CLS");
         j=737; *q=j;
-        printf("\n&p=%p p=%p *p=%d\n",&p,p,* p); //&�ܼ�=��} 
+        printf("\n&p=%p p=%p *p=%d\n",&p,p,* p);
         printf("\n&q=%p q=%p &j=%p *q=%d\n",&q,q,&j,*q);
         printf("\nsizeof(p)=%d sizeof(q)=%d sizeof(a)=%d\n\n\n"
                ,sizeof(p),sizeof(q),sizeof(a)); 
